4a99aeea574d18f3a0ec8aa80fc4057fdef31279
John Murray <heyjohnmurray@us.ibm.com>
2015-11-22 00:02:37 -0600

    just pushing some of the examples i've been working on

--- app/App.js
+++ app/App.js
@@ -2,13 +2,37 @@
 var React = require('react');
 
 var App = React.createClass({
-	render: function() {
-		var text = this.props.text;
-		return (
-			<h1>{text}</h1>
-		);
-	}
+  getInitialState: function() {
+    return {
+      text: 'this is initial state text',
+    };
+  },
 
+  // update is a custom function we've created
+  update: function(e) {
+    this.setState({text: e.target.value});
+  },
+
+	render: function() {
+		return (
+			<div>
+				<Widget text={this.state.text} update={this.update} />
+				<Widget text={this.state.text} update={this.update} />
+			</div>
+		);
+	},
+
+});
+
+var Widget  = React.createClass({
+	render: function() {
+    return (
+				<div>
+					<input type='text' onChange={this.props.update} />
+					<h1>{this.props.text}</h1>
+				</div>
+		);
+  },
 });
 
 module.exports = App;
